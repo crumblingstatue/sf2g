d914e3613a05bf51a5c90b1616e11b41450f9fc2
crumblingstatue <radiantstatue@gmail.com>
2025-04-25 20:23:50 +0200

    Don't try to find system freetype

--- SFML/src/SFML/Graphics/Font.cpp
+++ SFML/src/SFML/Graphics/Font.cpp
@@ -28,7 +28,7 @@
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/GLCheck.hpp>
 #include <SFML/System/Err.hpp>
-#include <ft2build.h>
+#include "ft2build.h"
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 #include FT_OUTLINE_H
